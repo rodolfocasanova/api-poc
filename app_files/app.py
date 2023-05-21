36a6587d962f58ff1b5cf73b8a2f2a5dddf5db7c
Rodolfo Casanova <6202719+rodolfocasanova@users.noreply.github.com>
2023-05-21 16:03:06 -0600

    fix manifest

--- app_files/app.py
+++ app_files/app.py
@@ -21,7 +21,7 @@ def hello():
     REQUEST_COUNT.labels(service='myapp', endpoint='/').inc()
 
     # Generate a random integer between a range
-    random_number = random.randint(1, 1)
+    random_number = random.randint(0, 2)
     print(random_number)
 
     # Convert the dictionary to JSON string
